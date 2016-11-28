d560b516f8241999ec46e07b10816c679180d685
timodie <timodie.addai@gmail.com>
2016-11-28 11:48:14 -0500

    failbook

--- server/src/server.js
+++ server/src/server.js
@@ -2,22 +2,58 @@
 var express = require('express');
 // Creates an Express server.
 var app = express();
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-res.send('Hello World!');
-});
-// Starts the server on port 3000!
-app.listen(3000, function () {
-console.log('Example app listening on port 3000!');
-});
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.text());
 // You run the server from `server`, so `../client/build` is `server/../client/build`.
 // '..' means "go up one directory", so this translates into `client/build`!
 app.use(express.static('../client/build'));
 
+/**
+* Get the feed data for a particular user.
+*/
+app.get('/user/:userid/feed', function(req, res) {
+var userid = req.params.userid;
+var fromUser = getUserIdFromToken(req.get('Authorization'));
+// userid is a string. We need it to be a number.
+// Parameters are always strings.
+var useridNumber = parseInt(userid, 10);
+if (fromUser === useridNumber) {
+// Send response.
+res.send(getFeedData(userid));
+} else {
+// 401: Unauthorized request.
+res.status(401).end();
+}
+});
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.text());
+/**
+* Get the user ID from a token. Returns -1 (an invalid ID)
+* if it fails.
+*/
+function getUserIdFromToken(authorizationLine) {
+try {
+// Cut off "Bearer " from the header value.
+var token = authorizationLine.slice(7);
+// Convert the base64 string to a UTF-8 string.
+var regularString = new Buffer(token, 'base64').toString('utf8');
+// Convert the UTF-8 string into a JavaScript object.
+var tokenObj = JSON.parse(regularString);
+var id = tokenObj['id'];
+// Check that id is a number.
+if (typeof id === 'number') {
+return id;
+} else {
+// Not a number. Return -1, an invalid ID.
+return -1;
+}
+} catch (e) {
+// Return an invalid ID.
+return -1;
+}
+}
+
+
+
 
 /**
 * Resolves a feed item. Internal to the server, since it's synchronous.
@@ -52,69 +88,14 @@ feedData.contents = feedData.contents.map(getFeedItemSync);
 // Return FeedData with resolved references.
 return feedData;
 }
-module.exports.getFeedData =getFeedData;
+//module.exports.getFeedData =getFeedData;
 
 
-/**
-* Get the feed data for a particular user.
-*/
-app.get('/user/:userid/feed', function(req, res) {
-var userid = req.params.userid;
-var fromUser = getUserIdFromToken(req.get('Authorization'));
-// userid is a string. We need it to be a number.
-// Parameters are always strings.
-var useridNumber = parseInt(userid, 10);
-if (fromUser === useridNumber) {
-// Send response.
-res.send(getFeedData(userid));
-} else {
-// 401: Unauthorized request.
-res.status(401).end();
-}
+
+
+
+
+// Starts the server on port 3000!
+app.listen(3000, function () {
+console.log('Example app listening on port 3000!');
 });
-
-
-/**
-* Get the user ID from a token. Returns -1 (an invalid ID)
-* if it fails.
-*/
-function getUserIdFromToken(authorizationLine) {
-try {
-// Cut off "Bearer " from the header value.
-var token = authorizationLine.slice(7);
-// Convert the base64 string to a UTF-8 string.
-var regularString = new Buffer(token, 'base64').toString('utf8');
-// Convert the UTF-8 string into a JavaScript object.
-var tokenObj = JSON.parse(regularString);
-var id = tokenObj['id'];
-// Check that id is a number.
-if (typeof id === 'number') {
-return id;
-} else {
-// Not a number. Return -1, an invalid ID.
-return -1;
-}
-} catch (e) {
-// Return an invalid ID.
-return -1;
-}
-}
-
-/*
- *Reverse functions commented out for now
-var util = require('./util.js');
-var reverseString = util.reverseString ;
-
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-// If the request came with text, then the text() middleware handled it
-// and made `req.body` a string.
-// Check that req.body is a string.
-if (typeof(req.body) === 'string') {
-var reversed = reverseString(req.body);
-res.send(reversed);
-} else {
-// POST did not contain a string. Send an error code back!
-res.status(400).end();
-}
-});*/
